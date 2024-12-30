0515fcfa059d1ee10aed21b7c6655836c6dcb82a
sidamodev <sidamo.dev@gmail.com>
2024-12-30 15:58:55 +0900

    init: next.js 프로젝트 생성

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "MyThreads",
+  description: "프론트엔드 심화 학습을 위한 Threads 클론 코딩"
+};
+
+export default function Home() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
